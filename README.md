8ff6890f624afee1e903567bf34fd27753a9f591
Philipp Grau <phgrau@zedat.fu-berlin.de>
2014-04-04 10:36:21 +0200

    Render in one row. Update documentation

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 racktables-stuff
 ================
 
-A small plugin for Racktables.org v0.20.5 only tested by me.
+A small plugin for Racktables.org v0.20.7 only tested by me.
 
-Just throw full_row_view.php in the the plugins directory of your
-racktables installation and open a rack row in the racktables web
-frontend, there should be a tab "Full Row View", enjoy.
+# Installation
+
+* just throw full_row_view.php, custom.php in the the plugins directory of your racktables installation
+* create a css directory in the plugins directory and copy custom.css over
+* you can apply a patch to wwwroot/inc/interface.php to rotate labels in vertical boxes (Blades and stuff)
+* open a rack row in the racktables web frontend, there should be a tab "Full Row View", enjoy.
 
 Feedback is welcome!
--- full_row_view.php
+++ full_row_view.php
@@ -1,73 +1,69 @@
 <?php
 //
 // Render a full row of racks view with server names
-// Version 0.2
+// Version 0.4
 //
 // Written by Philipp Grau <phgrau@zedat.fu-berlin.de>
 // 
 //
-// The purpose of this plugin is to render a web page with all racks of a row 
-// with clickable hostnames.
+// The purpose of this plugin is to render a web page with all racks of a row
+// with clickable hostnames and display the first tag.
 // Currently five racks will be grouped in a row.
 //
 // History
 // Version 0.1: Initial release (Tested on 0.20.4)
 //         0.2: Removed call of markupObjectProblems(), function was removed in 0.20.5
+//         0.3: Add patch for interface for rotated labels in layout style V (for bladecenters)
+//         0.4: Display rack row from left to right, no wrapping
 //
 // Installation:
-// 1)  Copy script to plugins folder as full_row_view.php
+// 1) Copy full_row_view.php to the plugins folder as full_row_view.php
+// 2) Copy cssloader.php to the plugins folder
+// 3) Copy custom.css to the plugins folder
+// 4) Apply patch to interface.php 
 
 $tab['row']['full_row_view'] = 'Full Row View';
 $tabhandler['row']['full_row_view'] = 'FullRowView';
 $ophandler['row']['full_row_view']['preparePrint'] ='preparePrint';
 
 // Set variables
-$frvVersion = "0.2";
-
+$frvVersion = "0.4";
 function preparePrint()
 {
     return showSuccess ("Created <a href=\"rowprint.php\">Print preview</a>!");
-}	      
-   
-	       
-// display« the import page.
+}
+
+
+// display the import page.
 function FullRowView()
 {
     if (isset($_REQUEST['row_id']))
       $row_id = $_REQUEST['row_id'];
     else
       $rack_id=1;
-    
+
     global $frvVersion;
     $rowData = getRowInfo ($row_id);
-    
-    // renderRack($rack_id);
-    
-    // echo 'huhu ' . $rackData['row_name'] . ' huhu ' . $rackData['row_id'];
+
     $cellfilter = getCellFilter();
     $rackList = filterCellList (listCells ('rack', $row_id), $cellfilter['expression']);
     // echo "<form method=post name=ImportObject action='?module=redirect&page=row&row_id=$row_id&tab=full_row_view&op=preparePrint'>";
     echo "<font size=1em color=gray>version ${frvVersion}&nbsp;</font>";
     // echo "<input type=submit name=got_very_fast_data value='Print view'>";
     // echo "</form>";
-    echo '<table><tr><td>';
+    echo '<table><tr><td nowrap="nowrap" valign="top">';
     $count = 1;
     foreach ($rackList as $rack) 
     {
 	// echo "<br>Schrank: ${rack['name']} ${rack['id']}";
 	// $rackData = spotEntity ('rack', ${rack['id']});
-	echo '<div class="phgrack" style="float: left; width: 240px">';
+	echo '<div class="phgrack" style="float: top; width: 240px">';
 	renderReducedRack("${rack['id']}");
 	echo '</div>';
-	if ($count % 5 === 0) {
-	    echo '</td></tr><tr><td>';
-	}
+        echo '</td><td nowrap="nowrap" valign="top">';
 
-	$count = $count + 1;
-				 
     }
     echo '</td></tr></table>';
-    // echo "<br>foo";
 }
 
 // This is form interface.php: renderRack
@@ -85,7 +81,7 @@ function renderReducedRack ($rack_id, $hl_obj_id = 0)
 	echo "<center><table border=0><tr valign=middle>";
 	echo '<td><h2>' . mkA ($rackData['name'], 'rack', $rackData['id']) . '</h2></td>';
 	echo "</h2></td></tr></table>\n";
-	echo "<table class=rack border=0 cellspacing=0 cellpadding=1>\n";
+	echo "<table class=rackphg border=0 cellspacing=0 cellpadding=1>\n";
 	echo "<tr><th width='10%'>&nbsp;</th><th width='20%'>Front</th>";
 	echo "<th width='50%'>Interior</th><th width='20%'>Back</th></tr>\n";
 	for ($i = $rackData['height']; $i > 0; $i--)
@@ -105,10 +101,19 @@ function renderReducedRack ($rack_id, $hl_obj_id = 0)
 			if (isset ($rackData[$i][$locidx]['rowspan']))
 				echo ' rowspan=' . $rackData[$i][$locidx]['rowspan'];
 			echo ">";
+
 			switch ($state)
 			{
 				case 'T':
 					printObjectDetailsForRenderRack($rackData[$i][$locidx]['object_id']);
+			                // TODO set background color based on the tag
+			                $o = spotEntity ('object',$rackData[$i][$locidx]['object_id']);
+			                while ( list ($key,$val) = each( $o['etags'] )) {
+					    echo "<div style='font: 8px Verdana,sans-serif; text-decoration:none; color=black'>";
+					    echo $val['tag'];
+					    echo "</div>";
+					    break;
+					}
 					break;
 				case 'A':
 					echo '<div title="This rackspace does not exist">&nbsp;</div>';
